a9dc38f4a17b4cfc60d4095c905ebcbfb6dfe487
Jean-Pierre-P <je.portino@duocuc.cl>
2025-11-30 18:21:58 -0300

    asdasd

--- usuarios/mvnw.cmd
+++ usuarios/mvnw.cmd
@@ -1,8 +1,4 @@
 <# : batch portion
-@REM
-if not exist "%JAVA_HOME%\bin\java.exe" (
-    set JAVA_HOME=
-)
 @REM ----------------------------------------------------------------------------
 @REM Licensed to the Apache Software Foundation (ASF) under one
 @REM or more contributor license agreements.  See the NOTICE file
